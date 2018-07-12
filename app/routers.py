560e2e626fcd2a9ec6fa230ade9629237bf8add8
userblog <rusin100195@gmail.com>
2018-07-12 17:43:24 +0300

    Add followers and followered

--- app/routers.py
+++ app/routers.py
@@ -51,6 +51,36 @@ def edit_profile():
 		form.about_me.data = current_user.about_me
 	return render_template('edit_profile.html', title='Edit profile', form=form)
 
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+	user = User.query.filter_by(username=username).first()
+	if user is None:
+		flash('User {} not found.'.format(username))
+		return redirect(url_for('index'))
+	if user==current_user:
+		flash('You cannot followed yourself.')
+		return redirect(url_for('user', username=username))
+	current_user.follow(user)
+	db.session.commit()
+	flash('Your are following {}.'.format(username))
+	return redirect(url_for('user', username=username))
+	
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+	user = User.query.filter_by(username=username).first()
+	if user is None:
+		flash('User {} not found.'.format(username))
+		return redirect(url_for('index'))
+	if user==current_user:
+		flash('You cannot unfollowed yourself.')
+		return redirect(url_for('user', username=username))
+	current_user.unfollow(user)
+	db.session.commit()
+	flash('Your are not following {}.'.format(username))
+	return redirect(url_for('user', username=username))
+
 @app.route('/logout')
 def logout():
 	logout_user()
--- db_add.py
+++ db_add.py
@@ -1,13 +1,13 @@
 from app.models import User
 from app import db
 
-user1 = User(username='Billy', email='billy@gmail.com')
+user1 = User(username='billy', email='billy@gmail.com')
 user1.set_password('billy')
 db.session.add(user1)
 db.session.commit()
 print('User1 OK')
 
-user2 = User(username='Nelly', email='nelly@gmail.com')
+user2 = User(username='nelly', email='nelly@gmail.com')
 user2.set_password('nelly')
 db.session.add(user2)
 db.session.commit()
